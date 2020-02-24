73d3767e2fe13ebd93f99c9f409a2b879465dbef
Thomas M. Higgins <61065565+thedeclineirl@users.noreply.github.com>
2020-02-24 16:46:54 +0000

    created clean.py & artists_erros.cvs
    clean.py
    new functions for cleaning up the error artists and categorizing non-music scrobbles
    
    artists_erros.cvs
    building a list of error artist names and correct names to be used in the artist cleaning function

---
+++ clean.py
@@ -0,0 +1,35 @@
+###
+'''
+---------------------------
+Name:		Thomas Higgins
+e-mail:		thomas.higgins.2@gmail.com
+---------------------------
+Created:	2020-02-24
+Edited:		2020-02-24
+---------------------------
+'''
+
+
+
+def get_artist_errors(input):
+	#	open file, read in data
+	#file = open(filename, encoding = "utf8")
+	dict = {}
+	file = open(input, 'r')
+	for line in file:
+		line.strip()
+		dict.append(line.split[0],line.split[1])
+
+	return dict	
+
+
+def get_comedy(input):
+	comedy = {}
+
+	return comedy
+
+def get_podcast(input):
+	podcast = {}
+
+	
+	return podcasts
